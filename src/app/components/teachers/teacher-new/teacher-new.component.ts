2095cddade16930e29351dd5e958a2c897501bca
JhordanBricenio <jhordan795@hotmail.com>
2025-06-26 17:29:15 -0500

    Insert api reniec students and modify form register

--- src/app/components/teachers/teacher-new/teacher-new.component.ts
+++ src/app/components/teachers/teacher-new/teacher-new.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-teacher-new',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './teacher-new.component.html',
   styleUrl: './teacher-new.component.css'
 })
 export class TeacherNewComponent {
+
+  form: FormGroup;
+  result: any = null;
+  error: string | null = null;
+
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.form = this.fb.group({
+      dni: ['', [Validators.required, Validators.pattern(/^\d{8}$/)]],
+      nombres: [''],
+      apellidoPaterno: [''],
+      apellidoMaterno: ['']
+    });
+  }
+
+  buscar() {
+    if (this.form.invalid) {
+      return;
+    }
+    const dni = this.form.get('dni')?.value;
+    this.userService.searchByDniApi(dni).subscribe({
+      next: (data) => {
+        this.form.patchValue({
+          nombres: data.nombres,
+          apellidoPaterno: data.apellidoPaterno,
+          apellidoMaterno: data.apellidoMaterno
+        });
+        this.error = null;
+      },
+      error: () => {
+        this.error = 'DNI no encontrado';
+        this.form.patchValue({
+          nombres: '',
+          apellidoPaterno: '',
+          apellidoMaterno: ''
+        });
+      }
+    });
+  }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GLOBAL } from './GLOBAL';
 import { Observable, catchError, throwError } from 'rxjs';
@@ -10,50 +10,58 @@ import { User } from '../models/user';
 export class UserService {
 
   public url;
-  
+
   constructor(private _http: HttpClient) {
-    this.url=GLOBAL.url;
+    this.url = GLOBAL.url;
   }
-  getUsers():Observable<User[]>{
-    return this._http.get<User[]>(this.url+'users');
+  getUsers(): Observable<User[]> {
+    return this._http.get<User[]>(this.url + 'users');
   }
 
   //createAlumnosAll()
-  createAlumnosAll(user:User):Observable<any>{
-    return this._http.post(this.url+'users',user);
+  createAlumnosAll(user: User): Observable<any> {
+    return this._http.post(this.url + 'users', user);
   }
 
   //getAlumnoById()
-  getAlumnoById(id:any):Observable<User>{
-    return this._http.get<User>(this.url+'users/'+id);
+  getAlumnoById(id: any): Observable<User> {
+    return this._http.get<User>(this.url + 'users/' + id);
   }
 
   getUserByDni(dni: string): Observable<User> {
     return this._http.post<User>(this.url + 'users/dni', { dni });
-    }
-  
+  }
+
 
   //updateAlumno()
-  updateAlumno(user:User):Observable<any>{
-    return this._http.put(this.url+'users/'+user.id,user);
+  updateAlumno(user: User): Observable<any> {
+    return this._http.put(this.url + 'users/' + user.id, user);
   }
 
   //deleteAlumno()
-  deleteAlumno(id:any):Observable<any>{
-    return this._http.delete(this.url+'users/'+id);
+  deleteAlumno(id: any): Observable<any> {
+    return this._http.delete(this.url + 'users/' + id);
   }
 
-  subirFoto(foto: File, id:any): Observable<HttpEvent<{}>> {
+  //obtenerDatosAPiDni
+
+  searchByDniApi(dni: string): Observable<any> {
+    console.log("DNI", dni);
+    return this._http.get<User>(this.url + `users/dni/${dni}`);
+  }
+
+
+  subirFoto(foto: File, id: any): Observable<HttpEvent<{}>> {
     let formData = new FormData();
     formData.append('file', foto);
     formData.append('id', id.toString());
-    const req = new HttpRequest('POST', this.url + 'users/upload', formData,{reportProgress: true});
+    const req = new HttpRequest('POST', this.url + 'users/upload', formData, { reportProgress: true });
     return this._http.request(req).pipe(
-       catchError(e => {
-          return throwError(()=>e);
-       })
+      catchError(e => {
+        return throwError(() => e);
+      })
     );
-  
 
- }
+
+  }
 }
